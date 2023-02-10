c50ee47bbe3bb238c5b7a3c4be7f330d2848a3fc
marcuswonder <marcus.bruce.smith@gmail.com>
2023-02-10 12:24:26 -0800

    Models created

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,106 @@
 from django.db import models
+from django.urls import reverse
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Artist(models.Model):
+    name = models.CharField(max_length=100)
+    members = models.CharField(max_length=150)
+    description = models.CharField(max_length=250)
+    
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.name} artist by {self.user}"
+    
+    # def get_absolute_url(self):
+        # return reverse('artist_detail', kwargs={'artist_id': self.id})
+    
+
+class Experience(models.Model):
+    MUSIC = 'MU'
+    VIDEO = 'VI'
+    SHOW = 'SH'
+    MERCHANDISE = 'ME'
+    SOCIAL_MEDIA = 'SO'
+    NEWS = 'NE'
+    EXPERIENCE_TYPE = [
+        (MUSIC, 'Music'),
+        (VIDEO, 'Video'),
+        (SHOW, 'Show'),
+        (MERCHANDISE, 'Merchandise'),
+        (SOCIAL_MEDIA, 'Social Media'),
+        (NEWS, 'News'),
+    ]
+    experience_type = models.CharField(
+        max_length=2,
+        choices=EXPERIENCE_TYPE
+    )
+
+    user_review = models.CharField(
+        max_length=500,
+        blank=True,
+        null=True
+    )
+    date_time = models.DateTimeField(
+        blank=True,
+        null=True
+    )
+
+    link = models.CharField(
+        max_length=200,
+        blank=True,
+        null=True
+    )
+
+    ALBUM = 'AL'
+    SINGLE = 'SI'
+    MIXTAPE = 'MI'
+    EP = 'EP'
+    FEATURE = 'FE'
+    MUSIC_TYPE = [
+        (ALBUM, 'ALBUM'),
+        (SINGLE, 'Single'),
+        (MIXTAPE, 'Mixtape'),
+        (EP, 'EP'),
+        (FEATURE, 'Features On'),
+    ]
+    music_type = models.CharField(
+        max_length=2,
+        choices=MUSIC_TYPE
+    )
+
+    show_venue_name = models.CharField(max_length=100)
+
+    artist = models.ForeignKey(Artist, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.experience_type} experience on {self.artist}"
+    
+    # def get_absolute_url(self):
+        # return reverse('experience_detail', kwargs={'experience_id': self.id})
+
+
+
+class Comment(models.Model):
+    comment = models.CharField(max_length=250)
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    experience = models.ForeignKey(Experience, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.user}'s comment on {self.experience}"
+    
+    # def get_absolute_url(self):
+        # return reverse('comment_detail', kwargs={'comment_id': self.id})
+
+
+class Photo(models.Model):
+    url = models.CharField(max_length=200)
+    artist = models.ForeignKey(Artist, on_delete=models.CASCADE)
+    experience = models.ForeignKey(Experience, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"Photo" 
+        # Needs some improvement - passing into two different models.
