d816aa366dbf045da6ff4c57b7e0a749aee9615f
nandobrain <prf.arturo@gmail.com>
2023-02-10 15:20:08 -0800

    views save

--- fanbass/settings.py
+++ fanbass/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -74,10 +75,21 @@ WSGI_APPLICATION = 'fanbass.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'fanbass',
+#     }
+# }
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'fanbass',
+        'NAME': '<DATABASE_NAME>',
+        'USER': '<USERNAME>',
+        'PASSWORD': '<v2_YOUR_PASSWORD_HERE>',
+        'HOST': 'db.bit.io',
+        'PORT': '5432',
     }
 }
 
--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic import DetailView
+from django.contrib.auth import login
+
+from .models import Experience
 
 # Create your views here.
+
+class AristCreate(CreateView):
+    model = Experience
+    field = '__all__'
+
+class ArtistDetail(DetailView):
+    model = Experience
+
+class ArtistUpdate(UpdateView):
+    model = Experience
+    field = '__all__'
+
+class AristDelete(DeleteView):
+    model = Experience
+    field= '/artist'
+
+
+
+
+
+
