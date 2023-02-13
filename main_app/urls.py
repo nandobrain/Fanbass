f474db22975f713e9c623b2ec5724391e82940cd
xandramiyo <aj.sumiyoshi@gmail.com>
2023-02-13 11:25:34 -0800

    templates complete for basic CRUD and registration

--- main_app/urls.py
+++ main_app/urls.py
@@ -5,12 +5,12 @@ from . import views
 
 
 urlpatterns = [
-   path('artist/', views.ArtistList.as_view(), name='artist_index'),
-   path('artist/<int:pk>/', views.ArtistDetail.as_view, name='artist_detail'),
-   path('artist/create/', views.ArtistCreate.as_view(), name='artist_create'),
-   path('artist/<int:pk>/update/', views.ArtistUpdate.as_view(), name='artist_update'),
-   path('artist/<int:pk>/delete/', views.ArtistDelete. as_view(), name='artist_delete'),
-   path('artist/<int:artist_id>/add_photo/', views.add_photo, name='add_photo'),
+   path('artists/', views.ArtistList.as_view(), name='artist_index'),
+   path('artists/<int:pk>/', views.ArtistDetail.as_view(), name='artist_details'),
+   path('artists/create/', views.ArtistCreate.as_view(), name='artist_create'),
+   path('artists/<int:pk>/update/', views.ArtistUpdate.as_view(), name='artist_update'),
+   path('artists/<int:pk>/delete/', views.ArtistDelete. as_view(), name='artist_delete'),
+   path('artists/<int:artist_id>/add_photo/', views.add_photo, name='add_photo'),
    path('accounts/signup/', views.signup, name='signup'),
    path('', views.home, name='home'),
 ]
